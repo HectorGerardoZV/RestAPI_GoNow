cb243c158731a963124e851a3de596710511551d
TorroSoft <torrozapata@gmail.com>
2021-10-08 17:13:55 -0700

    The model and the controller about User were finished and were tested in postmand

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -9,7 +9,46 @@ const addUser = async(req,res,next)=>{
         next();
     }
 }
-
-export{
-    addUser
+const findUser = async(req,res, next)=>{
+    const id = req.params.id;
+    try {
+        const user = await Users.findById(id);
+        res.json(user);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
 }
+const findAllUsers = async(req,res, next)=>{
+    try {
+        const users =await Users.find();
+        res.json(users);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+const updateUser = async(req,res, next)=>{
+    const id = req.params.id;
+    const updateUser = req.body;
+   try {
+       const user = await Users.findOneAndUpdate({_id: id}, updateUser,{new: true});
+       res.json(user);
+   } catch (error) {
+       console.log(error);
+       next();
+   }
+}
+const deleteUser = async(req,res, next)=>{
+   const id = req.params.id;
+
+    try {
+     await Users.findOneAndDelete({_id: id});
+     res.json({message: "The client were deleted successfuly"});
+   } catch (error) {
+       console.log(error);
+       next();
+   }
+}
+
+export{addUser,findUser,findAllUsers,updateUser,deleteUser}
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import {addUser} from "../controllers/UsersController.js";
+import {addUser,findUser,findAllUsers,updateUser,deleteUser} from "../controllers/UsersController.js";
 const router = express.Router();
 
 //Users routes
 router.post("/users",addUser);
+router.get("/users",findAllUsers);
+router.get("/users/:id",findUser);
+router.put("/users/:id",updateUser);
+router.delete("/users/:id",deleteUser);
 
 
 export default router;
